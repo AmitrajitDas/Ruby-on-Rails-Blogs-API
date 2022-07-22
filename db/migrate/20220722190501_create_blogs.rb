dece108c6db6985ab248f36bb8eadd01dadcd731
AmitrajitDas <amitrajitdas31@gmail.com>
2022-07-23 02:39:51 +0530

    new: blogs schema

---
+++ db/migrate/20220722190501_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
