fce8864a14b3d0bad6fb4571b70c914392090b47
AmitrajitDas <amitrajitdas31@gmail.com>
2022-07-23 02:40:16 +0530

    update: blog to user associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :users, param: :_username
+
+  resources :users, param: :_username do
+    resources :blogs
+  end
+
   post "/login", to: "authentication#login"
+  
   get '/*a', to: 'application#not_found'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_090612) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_22_190147) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
