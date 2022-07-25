6fb4d84aeebc3d93b70de20c0a5e9849897c37d1
AmitrajitDas <amitrajitdas31@gmail.com>
2022-07-25 15:56:32 +0530

    new: blog schema and migrations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, length: { minimum: 5 }, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_190147) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_22_190501) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_190147) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "users"
 end
