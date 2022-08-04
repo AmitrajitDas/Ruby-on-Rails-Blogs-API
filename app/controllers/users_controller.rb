1954f4a1fe1edb9913e088f74e4e80ecbcd53c8b
AmitrajitDas <amitrajitdas31@gmail.com>
2022-08-04 13:23:55 +0530

    fix: on delete cascade

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def find_user
-    @user = User.find_by_username!(params[:_username])
+    @user = User.find_by_id!(params[:id])
     rescue ActiveRecord::RecordNotFound
       render json: { errors: 'User not found' }, status: :not_found
   end
